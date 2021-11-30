fd6463cc6b2197236be0da9fdce8a1b91f63a0af
Triggs125 <tannerdriggers@gmail.com>
2021-11-29 23:40:44 -0600

    Added Navbar and background color rotations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
+
+import {
+  Container,
+  Nav,
+  Navbar,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const backgroundColors = [
+  "bg-primary",
+  "bg-secondary",
+  "bg-success",
+  "bg-warning",
+  "bg-danger",
+  "bg-info",
+];
+
+const Home = () => <span>Home</span>;
+
+const About = () => <span>About</span>;
+
+const Users = () => <span>Users</span>;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageBackground: "bg-secondary"
+    }
+  }
+
+  componentDidMount() {
+    let i = 0;
+    setInterval(() => {
+      this.setState({ pageBackground: backgroundColors[i]});
+      if (i >= backgroundColors.length - 1) i = -1;
+      i += 1;
+    }, 5000);
+  }
+
+  render() {
+    const { pageBackground } = this.state;
+
+    return (
+      <MemoryRouter>
+        <Navbar collapseOnSelect bg="dark" variant="dark" expand="md" sticky="top">
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Brand className="nav-name">
+            <LinkContainer to="/">
+              <h1>Tanner Driggers</h1>
+            </LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="p-3 nav-links">
+              <Nav.Link>
+                <LinkContainer to="/">
+                  <h5>Home</h5>
+                </LinkContainer>
+              </Nav.Link>
+              <Nav.Link>
+                <LinkContainer to="/about">
+                  <h5>About</h5>
+                </LinkContainer>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container className={`p-3 m-auto ${pageBackground} outer-container`}>
+          <Container className="p-5 m-auto align-contents-center bg-light rounded-3">
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Container>
+        </Container>
+      </MemoryRouter>
+    );
+  }
+};
 
 export default App;
